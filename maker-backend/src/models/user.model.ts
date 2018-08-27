3457f296e1d892948e3de03b2cf3a7965ad7f114
NoMoreViolence <ljh86029926@gmail.com>
2018-08-27 16:00:37 +0900

    Backend-Model file naming way is changed

---
+++ maker-backend/src/models/user.model.ts
@@ -0,0 +1,33 @@
+import { Table, Column, Model, AllowNull, DataType, CreatedAt, UpdatedAt, DeletedAt, Unique } from 'sequelize-typescript';
+
+@Table({ timestamps: true })
+class User extends Model<User> {
+  @Column(DataType.STRING({ length: 50 }))
+  @Unique
+  @AllowNull(false)
+  public username: string;
+
+  @Column(DataType.STRING({ length: 100 }))
+  @Unique
+  @AllowNull(false)
+  public email: string;
+
+  @Column(DataType.STRING({ length: 50 }))
+  @AllowNull(false)
+  public password: string;
+
+  @Column(DataType.INTEGER)
+  @AllowNull(false)
+  public salt: number;
+
+  @CreatedAt
+  public creationDate: Date;
+
+  @UpdatedAt
+  public updatedOn: Date;
+
+  @DeletedAt
+  public deletionDate: Date;
+}
+
+export { User };
