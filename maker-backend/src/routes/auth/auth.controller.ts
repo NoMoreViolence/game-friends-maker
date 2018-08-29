5fc7402d8b31868bff067a8bb97a346ab85b66e1
NoMoreViolence <ljh86029926@gmail.com>
2018-08-29 18:18:50 +0900

    Backend-Working

--- maker-backend/src/routes/auth/auth.controller.ts
+++ maker-backend/src/routes/auth/auth.controller.ts
@@ -1,55 +1,63 @@
 import { Request, Response } from 'express';
-import { FilteredModelAttributes } from 'sequelize-typescript';
-import { DatabaseError } from 'sequelize';
-import { User, Email } from 'db/models';
-import { Observable, Subscriber } from 'rxjs';
 import lib from 'src/lib';
+import * as models from 'db/models';
+import Sequelize from 'db';
+
+const { User } = models;
 const { mailer, salt, regex } = lib;
 
-interface CheckEmail {
-  email: string;
+/* GET */
+// Username, E-mail duplication check function
+export const checkDuplication = (req: Request, res: Response): Response => {
+  return res.json({
+    success: true,
+    message: 'Duplication check succeeded !'
+  });
+};
+
+/* Post */
+interface Register {
   username: string;
-  AuthCode: number;
+  email: string;
+  password: string;
+  salt: number;
 }
-export const CheckEmail = (req: Request, res: Response): Response | any => {
-  const { email, username } = req.query;
+export const register = (req: Request, res: Response): void => {
+  const { username, email, password } = req.body;
 
-  const checkEmail = (value: CheckEmail): Promise<CheckEmail> | any =>
-    value.email
-      ? regex.emailRegex.test(value.email)
-        ? Promise.resolve(value)
-        : Promise.reject(new Error('Not formatted'))
-      : Promise.reject(new Error('No email data here !'));
+  // Value validation check
+  const checkRegex = (value: Register): Promise<Register> =>
+    regex.usernameRegex.test(value.username) && regex.emailRegex.test(value.email) && regex.passwordRegex.test(value.password)
+      ? Promise.resolve(value)
+      : Promise.reject(new Error('Validation error !'));
 
-  const findEmail = (value: CheckEmail): Promise<CheckEmail> =>
-    new Promise((resolve, reject) => {
-      User.findOne({ where: { email: value.email } })
-        .then((data: User) => {
-          if (data) {
-            return reject(new Error('Duplicate Email !'));
-          }
-          return resolve(value);
-        })
-        .catch((err: DatabaseError) => {
-          return reject(new Error('Database Error !'));
-        });
-    });
+  const doubleCheck = async (value: Register): Promise<Register> => {
+    const [checkUsername, checkEmail] = await Promise.all([
+      User.findOne({ where: Sequelize.or([{ username: value.username }, { email: value.email }]) })
+    ]);
 
-  const createEmailQuery = (value: CheckEmail) =>
-    new Promise((resolve, rject) => {
-      Email.upsert({ email: value.email });
-    });
+    return checkUsername && checkEmail ? Promise.resolve(checkUsername) : Promise.reject(new Error(''));
+  };
 
-  const sendMail = (value: CheckEmail) => {};
+  const passwordEncryption = (value: Register): Promise<Register> => {
+    return Promise.resolve(value);
+  };
 
-  const returnToClient = (value: CheckEmail) => {
-    res.json({
-      success: true,
-      message: 'First email checking is valid !',
-      email: value.email
+  const addUser = (value: Register): Promise<Register> => {
+    return new Promise((resolve, reject) => {
+      const user = User.build({ username: value.username, email: value.email });
+      resolve(user);
     });
   };
 
+  const responseToClient = (value: Register) => {
+    res.json({
+      success: true,
+      message: 'Register is succeed !'
+    });
+  };
+
+  // Error handler
   const onError = (err: Error) => {
     res.status(409).json({
       success: false,
@@ -57,41 +65,11 @@ export const CheckEmail = (req: Request, res: Response): Response | any => {
     });
   };
 
-  checkEmail({ email, username, AuthCode: 0 })
-    .then(findEmail)
+  // Register
+  checkRegex({ username, email, password, salt: 0 })
+    .then(doubleCheck)
+    .then(passwordEncryption)
+    .then(addUser)
+    .then(responseToClient)
     .catch(onError);
 };
-
-// setTimeout(() => {
-//   User.create({
-//     username: 'awedwdfqwdqwdawef',
-//     password: 'awjefwdqwdqwdwdwdioawef',
-//     email: 'awefjawwdqwdqwdwdef',
-//     salt: 1231112
-//   })
-//     .then(res => console.log(res.dataValues))
-//     .catch(err => console.log(err.message));
-// }, 1000);
-
-// setTimeout(async () => {
-//   await User.create({
-//     username: 'awefawef',
-//     password: 'awjefioawef',
-//     email: 'awefjawef',
-//     salt: 123
-//   });
-
-//   User.findOne({
-//     where: {
-//       username: 'awefawef'
-//     }
-//   })
-//     .then(res => {
-//       console.log(res.dataValues);
-//       console.log('찾았다');
-//     })
-//     .catch(err => {
-//       console.log(err);
-//       console.log('아니다');
-//     });
-// }, 1000);
--- maker-backend/src/routes/auth/index.ts
+++ maker-backend/src/routes/auth/index.ts
@@ -1,9 +1,14 @@
 import * as express from 'express';
 import * as controller from './auth.controller';
+import middelware from 'src/middleware';
 
 const Router = express.Router();
 
-Router.get('/email', controller.CheckEmail);
+Router.use('/duplication', middelware.checkUserInfoMiddleware);
+Router.get('/duplication', controller.checkDuplication);
+
+// Router.get('/email', controller.CheckEmail);
+
 // router.get('/register', controller.register); // Register
 // router.post('/login', controller.login); // Login
 // router.post('/check', controller.check); // Token Check
