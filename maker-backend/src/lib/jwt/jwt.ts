91f87ba3c59a4066562446df82614ad6e11ded15
NoMoreViolence <ljh86029926@gmail.com>
2018-09-02 09:29:16 +0900

    Backend-Complete jwt lib

--- maker-backend/src/lib/jwt/jwt.ts
+++ maker-backend/src/lib/jwt/jwt.ts
@@ -11,21 +11,13 @@ const createJWT = (id: number, username: string, email: string) =>
     { algorithm: 'HS512', expiresIn: 2628000 * 2, issuer: 'GameFriendsMaker<Ji-Hoon LEE>' }
   );
 
-interface Decoded {
-  id: number;
-  username: string;
-  email: string;
-  iat: number;
-  exp: number;
-  iss: string;
-}
-const decodeJWT = (token: string): Promise<Decoded> =>
+const decodeJWT = (token: string): Promise<string | object> =>
   new Promise((resolve, reject) =>
     jwt.verify(
       token,
       process.env.JWT_KEY,
       { algorithms: ['HS512'] },
-      (err, decoded) => (err ? reject(new Error(err.name)) : resolve(decoded))
+      (err: jwt.VerifyErrors, decoded) => (err ? reject(new Error(err.name)) : resolve(decoded))
     )
   );
 
