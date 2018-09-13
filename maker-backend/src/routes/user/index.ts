45cdfeddabe762c7a84902d6487b4ce14283b52d
NoMoreViolence <ljh86029926@gmail.com>
2018-09-13 17:57:46 +0900

    Backend-Change username api (Working)

--- maker-backend/src/routes/user/index.ts
+++ maker-backend/src/routes/user/index.ts
@@ -4,4 +4,7 @@ import middleware from 'src/middleware';
 
 const Router = express.Router();
 
+Router.use('/username', middleware.checkCookie);
+Router.patch('/username', controller.changeUsername);
+
 export default Router;
--- maker-backend/src/routes/user/user.controller.ts
+++ maker-backend/src/routes/user/user.controller.ts
@@ -6,13 +6,62 @@ import lib, { EncryptoPassword } from 'src/lib';
 const { Op } = Sequelize;
 const { salt, regex, validation, encrypto, jwt } = lib;
 
+interface ChangeUsername {
+  username: string;
+  newUsername: string;
+}
 export const changeUsername = (req: Request, res: Response) => {
-  const { willChange } = req.body;
+  const { username } = res.locals;
+  const { newUsername } = req.body;
 
+  // Null check
+  const checkNull = (value: ChangeUsername) =>
+    value.newUsername === undefined
+      ? Promise.reject(new Error('Violation error !'))
+      : value.newUsername.trim() === ''
+        ? Promise.reject(new Error('Violation error !'))
+        : Promise.resolve({ ...value, newUsername: value.newUsername.trim() });
+
+  // Update name
+  const updateUsername = (value: ChangeUsername) => {
+    return new Promise((resolve, reject) => {
+      User.update({ username: value.newUsername }, { where: { username: value.username } })
+        .then(data => {
+          console.log(data);
+          console.log('dat');
+          resolve(data);
+        })
+        .catch(err => {
+          reject(new Error('Server error'));
+          console.log(err.message);
+          console.log('err');
+        });
+    });
+  };
+
+  // Response
+  const responseToClient = (value: ChangeUsername) => {
+    res.json({
+      success: true,
+      message: 'Username change success !',
+      value: {
+        before: value.username,
+        after: value.newUsername
+      }
+    });
+  };
+
+  // Error handler
   const onError = (err: Error) => {
     res.status(409).json({
       success: false,
       message: err.message
     });
   };
+
+  // Promise
+  checkNull({ username, newUsername })
+    .then(updateUsername)
+    .then(responseToClient)
+    .catch(onError);
 };
