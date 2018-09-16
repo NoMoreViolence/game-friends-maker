88530b94f7d24ab9d0b971256018928f01b7ef17
NoMoreViolence <ljh86029926@gmail.com>
2018-09-16 18:18:04 +0900

    Backend-Allow Uppercase and Lowercase

--- maker-backend/src/lib/regex/username.ts
+++ maker-backend/src/lib/regex/username.ts
@@ -1,4 +1,4 @@
 // 3 - 16 length, allow '_', '-', number, alphabet
-const usernameRegex: RegExp = /^[a-z0-9_-]{3,16}$/;
+const usernameRegex: RegExp = /^[A-za-z0-9_-]{3,16}$/;
 
 export { usernameRegex };
