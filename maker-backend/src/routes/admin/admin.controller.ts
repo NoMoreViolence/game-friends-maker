ab762053a04425f5bf6decc8617f519d698bbf15
NoMoreViolence <ljh86029926@gmail.com>
2018-09-23 01:16:30 +0900

    Backend-Add add game info route

--- maker-backend/src/routes/admin/admin.controller.ts
+++ maker-backend/src/routes/admin/admin.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import { DatabaseError } from 'sequelize';
-import Sequelize, { User, Game, AllGame } from 'db';
+import Sequelize, { User, Game, AllGame, AllGenreGame } from 'db';
 import lib, { EncryptoPassword } from 'src/lib';
 import { JsonWebTokenError } from 'jsonwebtoken';
 
@@ -8,26 +8,57 @@ const { Op } = Sequelize;
 const { salt, regex, validation, encrypto, jwt } = lib;
 
 interface AddGame {
-  id: string;
+  id: number;
+  gameId: number;
   gamename: string;
-  genre: string;
+  genre: number[];
   window: boolean;
   mac: boolean;
+  xbox: boolean;
   ps: boolean;
   nswitch: boolean;
+  android: boolean;
+  ios: boolean;
 }
 export const addGame = (req: Request, res: Response) => {
   const { id } = res.locals;
   const { gamename } = req.params;
-  const { genre, window, mac, xbox, ps, nswitch } = req.body;
+  const { genre, window, mac, xbox, ps, nswitch, android, ios } = req.body;
 
-  const checkAdmin = (value: AddGame): Promise<AddGame> => {
-    //
-    return Promise.resolve(value);
-  };
+  const checkValue = (value: AddGame): Promise<AddGame> =>
+    new Promise(
+      (resolve, reject) => (Array.isArray(value.genre) ? resolve(value) : reject(new Error('There is a wrong value (genre)')))
+    );
 
-  const checkNull = (value: AddGame): Promise<AddGame> => {
-    //
+  const checkAdmin = (value: AddGame): Promise<AddGame> =>
+    value.id === 1 ? Promise.resolve(value) : Promise.reject(new Error('You are not admin !'));
+
+  const createGame = (value: AddGame): Promise<AddGame> =>
+    new Promise((resolve, reject) => {
+      AllGame.create({
+        gamename: value.gamename,
+        window: value.window,
+        mac: value.mac,
+        xbox: value.xbox,
+        ps: value.ps,
+        nswitch: value.nswitch,
+        android: value.android,
+        ios: value.ios
+      })
+        .then(data => resolve({ ...value, gameId: data.dataValues.id }))
+        .catch((err: DatabaseError) => reject(new Error(err.message)));
+    });
+
+  const addGenre = (value: AddGame): Promise<AddGame> => {
+    value.genre.map((object: number, i: number) =>
+      AllGenreGame.create({
+        allid: value.gameId,
+        genreid: object
+      })
+        .then(() => console.log(`${i}th genre add success !`))
+        .catch(() => console.log(`${i}th genre add failure !`))
+    );
+
     return Promise.resolve(value);
   };
 
@@ -44,4 +75,11 @@ export const addGame = (req: Request, res: Response) => {
       success: false,
       message: err.message
     });
+
+  checkValue({ id, gameId: -1, gamename, genre, window, mac, xbox, ps, nswitch, android, ios })
+    .then(checkAdmin)
+    .then(createGame)
+    .then(addGenre)
+    .then(responseToClient)
+    .catch(onError);
 };
--- maker-backend/src/routes/admin/index.ts
+++ maker-backend/src/routes/admin/index.ts
@@ -8,8 +8,10 @@ const Router = express.Router();
 Router.use('/game/:gamename', middleware.checkCookie);
 Router.post('/game/:gamename', controller.addGame);
 // Game Change
+// Router.use('/game/:gamename', middleware.checkCookie);
 // Router.patch('/game/:game', controller.gameChange);
 // Game delete
+// Router.use('/game/:gamename', middleware.checkCookie);
 // Router.delete('/game/:game', controller.gameDelete);
 
 export default Router;
