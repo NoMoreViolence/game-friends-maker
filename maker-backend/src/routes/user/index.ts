bf217139ef2b682cd6530060994affa1a1576585
NoMoreViolence <ljh86029926@gmail.com>
2018-09-23 01:11:24 +0900

    Backend-Change url alike rest api

--- maker-backend/src/routes/user/index.ts
+++ maker-backend/src/routes/user/index.ts
@@ -4,20 +4,20 @@ import middleware from 'src/middleware';
 
 const Router = express.Router();
 
-// Change password
-Router.use('/change/password', middleware.checkCookie);
-Router.patch('/change/password', controller.changeUserPassword);
-
-// Change Username & Email
-Router.use('/change/:what', middleware.checkCookie);
-Router.patch('/change/:what', controller.changeUserInfo);
+// Check email verify code
+Router.use('/email', middleware.checkCookie);
+Router.post('/email', controller.checkEmailVerifyCode);
 
 // Send email verify code
 Router.use('/email', middleware.checkCookie);
 Router.get('/email', controller.requestEmailVerifyCode);
 
-// Check email verify code
-Router.use('/email', middleware.checkCookie);
-Router.post('/email', controller.checkEmailVerifyCode);
+// Change password
+Router.use('/password', middleware.checkCookie);
+Router.patch('/password', controller.changeUserPassword);
+
+// Change Username & Email
+Router.use('/:what', middleware.checkCookie);
+Router.patch('/:what', controller.changeUserInfo);
 
 export default Router;
