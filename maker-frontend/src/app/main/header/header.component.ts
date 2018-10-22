324395c0de239100ddb0e0837ee0acf1be47d8b8
NoMoreViolence <ljh86029926@gmail.com>
2018-10-22 22:30:03 +0900

    Frontend-Folder change

--- maker-frontend/src/app/main/header/header.component.ts
+++ maker-frontend/src/app/main/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
