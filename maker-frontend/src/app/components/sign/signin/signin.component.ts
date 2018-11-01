c59e225caaf909d4f1eee8a91ee4c762e80080a2
NoMoreViolence <ljh86029926@gmail.com>
2018-11-02 00:24:34 +0900

    Frontend-Sign in components

--- maker-frontend/src/app/components/sign/signin/signin.component.ts
+++ maker-frontend/src/app/components/sign/signin/signin.component.ts
@@ -1,22 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { fromEvent } from 'rxjs';
+import { debounceTime, catchError, tap } from 'rxjs/operators';
 
+import lib from 'src/app/lib';
 import { User } from '../../../ngrx/models';
 import { SignActions } from '../../../ngrx/actions';
-import { SignIn } from '../../../ngrx/actions/sign.actions';
 
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
   styleUrls: ['./signin.component.scss']
 })
-export class SigninComponent implements OnInit {
+export class SigninComponent implements AfterViewInit {
+  public showEmailError = true;
+  public showPasswordError = true;
+  @ViewChild('email')
+  email: ElementRef<HTMLInputElement>;
+  @ViewChild('password')
+  password: ElementRef<HTMLInputElement>;
+
   constructor(private store: Store<User>) {}
 
-  ngOnInit() {}
+  ngAfterViewInit() {
+    fromEvent(this.email.nativeElement, 'input')
+      .pipe(
+        tap(x => (this.showEmailError = true)),
+        debounceTime(800)
+      )
+      .subscribe(data => {
+        this.showEmailError = lib.emailRegex.test(this.email.nativeElement.value);
+      });
+
+    fromEvent(this.password.nativeElement, 'input')
+      .pipe(
+        tap(x => (this.showEmailError = true)),
+        debounceTime(800)
+      )
+      .subscribe(data => {
+        this.showPasswordError = lib.passwordRegex.test(this.password.nativeElement.value);
+      });
+  }
 
   private signIn = (email: HTMLInputElement, password: HTMLInputElement) => {
-    this.store.dispatch(new SignIn({ email: email.value, password: password.value }));
+    this.store.dispatch(new SignActions.SignIn({ email: email.value, password: password.value }));
     email.value = '';
     password.value = '';
   }
