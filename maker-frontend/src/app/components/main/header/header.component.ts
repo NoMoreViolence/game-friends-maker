c8ee636197b9656ed972c5f8405fd16d8d9e4539
NoMoreViolence <ljh86029926@gmail.com>
2018-11-04 13:05:21 +0900

    Frontend-Logout & View different menu when sign or not sign

--- maker-frontend/src/app/components/main/header/header.component.ts
+++ maker-frontend/src/app/components/main/header/header.component.ts
@@ -1,16 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
+import { Observable } from 'rxjs';
+import { AppState, User, SignActions } from '../../../ngrx';
+import { Store } from '@ngrx/store';
+import { ToastrService } from '../../../../../node_modules/ngx-toastr';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
-  constructor(private router: Router, private translate: TranslateService) {}
+export class HeaderComponent implements OnInit, AfterViewInit {
+  public user: Observable<User>;
+
+  constructor(private store: Store<AppState>, private router: Router, private toast: ToastrService, private translate: TranslateService) {
+    this.user = this.store.select('user');
+    this.user.subscribe(userData => {
+      console.log(userData);
+    });
+  }
+
+  public logout = () => {
+    this.store.dispatch(new SignActions.Logout(null));
+    localStorage.removeItem('token');
+    this.router.navigateByUrl('/main');
+    this.translate.get('Header.logout').subscribe(comment => this.toast.info(comment));
+  }
+
+  public changeLang = (value: HTMLSpanElement) => this.translate.use(value.innerText);
 
   ngOnInit() {}
 
-  public changeLang = (value: HTMLSpanElement) => this.translate.use(value.innerText);
+  ngAfterViewInit() {}
 }
