c06fcd754db6653173186531adacb71b89c00444
NoMoreViolence <ljh86029926@gmail.com>
2018-11-04 16:58:31 +0900

    Frontend-Complete sign up component

--- maker-frontend/src/app/components/sign/signup/signup.component.ts
+++ maker-frontend/src/app/components/sign/signup/signup.component.ts
@@ -1,15 +1,117 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { TranslateService } from '@ngx-translate/core';
+import { Observable, combineLatest, fromEvent } from 'rxjs';
+import { first, tap, debounceTime } from 'rxjs/operators';
+import { ToastrService } from 'ngx-toastr';
+import { AppState, User, SignActions } from 'src/app/ngrx';
+import lib from 'src/app/lib';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss']
 })
-export class SignupComponent implements OnInit {
+export class SignupComponent implements AfterViewInit {
+  private nError = true;
+  private eError = true;
+  private pError = true;
+  private rpError = true;
+  @ViewChild('name')
+  private username: ElementRef<HTMLInputElement>;
+  @ViewChild('em')
+  private email: ElementRef<HTMLInputElement>;
+  @ViewChild('pw')
+  private password: ElementRef<HTMLInputElement>;
+  @ViewChild('rpw')
+  private rpassword: ElementRef<HTMLInputElement>;
+  private user: Observable<User>;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private store: Store<AppState>, private router: Router, private toast: ToastrService, private trnaslate: TranslateService) {
+    this.user = this.store.select('user');
   }
 
+  ngAfterViewInit() {
+    this.user.pipe(first()).subscribe(userData => {
+      if (userData.success === true) {
+        combineLatest(this.router.navigateByUrl('/main'), this.trnaslate.get('Sign.up.already'), (route, comment) =>
+          this.toast.info(comment)
+        ).subscribe();
+      } else {
+        fromEvent(this.username.nativeElement, 'input')
+          .pipe(
+            tap(x => (this.nError = true)),
+            debounceTime(600)
+          )
+          .subscribe(data => {
+            if (this.username.nativeElement.value === '') {
+              this.nError = true;
+            } else {
+              this.nError = lib.usernameRegex.test(this.username.nativeElement.value);
+            }
+          });
+        fromEvent(this.email.nativeElement, 'input')
+          .pipe(
+            tap(x => (this.eError = true)),
+            debounceTime(600)
+          )
+          .subscribe(data => {
+            if (this.email.nativeElement.value === '') {
+              this.eError = true;
+            } else {
+              this.eError = lib.emailRegex.test(this.email.nativeElement.value);
+            }
+          });
+        fromEvent(this.password.nativeElement, 'input')
+          .pipe(
+            tap(x => (this.pError = true)),
+            debounceTime(600)
+          )
+          .subscribe(data => {
+            if (this.password.nativeElement.value === '') {
+              this.pError = true;
+            } else {
+              this.pError = lib.passwordRegex.test(this.password.nativeElement.value);
+              this.rpError = this.rpassword.nativeElement.value === this.password.nativeElement.value;
+            }
+          });
+        fromEvent(this.rpassword.nativeElement, 'input')
+          .pipe(
+            tap(x => (this.rpError = true)),
+            debounceTime(600)
+          )
+          .subscribe(data => {
+            if (this.rpassword.nativeElement.value === '') {
+              this.rpError = true;
+            } else {
+              this.rpError = this.rpassword.nativeElement.value === this.password.nativeElement.value;
+            }
+          });
+      }
+    });
+  }
+
+  private signUp = (username: HTMLInputElement, email: HTMLInputElement, password: HTMLInputElement, rpassword: HTMLInputElement) => {
+    if (
+      !this.nError ||
+      !this.eError ||
+      !this.pError ||
+      !this.rpError ||
+      username.value === '' ||
+      email.value === '' ||
+      password.value === '' ||
+      rpassword.value === ''
+    ) {
+      this.toast.error('Not right value !');
+    } else {
+      this.store.dispatch(
+        new SignActions.SignUp({
+          username: username.value,
+          email: email.value,
+          password: password.value
+        })
+      );
+    }
+  }
 }
