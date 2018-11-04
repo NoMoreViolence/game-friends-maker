7998e0567f6886570eab6d7cb818112e309322b5
NoMoreViolence <ljh86029926@gmail.com>
2018-11-05 02:28:14 +0900

    Frontend-Add double-check part

--- maker-frontend/src/app/components/sign/signup/signup.component.ts
+++ maker-frontend/src/app/components/sign/signup/signup.component.ts
@@ -1,9 +1,10 @@
 import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { TranslateService } from '@ngx-translate/core';
-import { Observable, combineLatest, fromEvent } from 'rxjs';
-import { first, tap, debounceTime } from 'rxjs/operators';
+import { Observable, combineLatest, fromEvent, forkJoin } from 'rxjs';
+import { first, tap, debounceTime, map } from 'rxjs/operators';
 import { ToastrService } from 'ngx-toastr';
 import { AppState, User, SignActions } from 'src/app/ngrx';
 import lib from 'src/app/lib';
@@ -14,6 +15,11 @@ import lib from 'src/app/lib';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements AfterViewInit {
+  private usernameCheck = false;
+  private usernamePending = false;
+  private emailCheck = false;
+  private emailPending = false;
+  // Error: false, success: true
   private nError = true;
   private eError = true;
   private pError = true;
@@ -28,20 +34,29 @@ export class SignupComponent implements AfterViewInit {
   private rpassword: ElementRef<HTMLInputElement>;
   private user: Observable<User>;
 
-  constructor(private store: Store<AppState>, private router: Router, private toast: ToastrService, private trnaslate: TranslateService) {
+  constructor(
+    private store: Store<AppState>,
+    private router: Router,
+    private http: HttpClient,
+    private toast: ToastrService,
+    private translate: TranslateService
+  ) {
     this.user = this.store.select('user');
   }
 
   ngAfterViewInit() {
     this.user.pipe(first()).subscribe(userData => {
       if (userData.success === true) {
-        combineLatest(this.router.navigateByUrl('/main'), this.trnaslate.get('Sign.up.already'), (route, comment) =>
+        combineLatest(this.router.navigateByUrl('/main'), this.translate.get('Sign.up.already'), (route, comment) =>
           this.toast.info(comment)
         ).subscribe();
       } else {
         fromEvent(this.username.nativeElement, 'input')
           .pipe(
-            tap(x => (this.nError = true)),
+            tap(x => {
+              this.usernameCheck = false;
+              this.nError = true;
+            }),
             debounceTime(600)
           )
           .subscribe(data => {
@@ -53,7 +68,10 @@ export class SignupComponent implements AfterViewInit {
           });
         fromEvent(this.email.nativeElement, 'input')
           .pipe(
-            tap(x => (this.eError = true)),
+            tap(x => {
+              this.emailCheck = false;
+              this.eError = true;
+            }),
             debounceTime(600)
           )
           .subscribe(data => {
@@ -92,6 +110,27 @@ export class SignupComponent implements AfterViewInit {
     });
   }
 
+  private doubleCheck = (element: HTMLInputElement, what: string) => {
+    if ((what === 'username' && this[`${what}Pending`] === false) || (what === 'email' && this[`${what}Pending`] === false)) {
+      this[`${what}Pending`] = true;
+
+      this.http.get(`/api/auth/duplication/${what}?checkvalue=${element.value}`).subscribe(
+        res => {
+          this[`${what}Pending`] = false;
+          this[`${what}Check`] = true;
+          this.translate.get('Sign.up.checks').subscribe(comment => this.toast.info(comment));
+        },
+        err => {
+          this[`${what}Pending`] = false;
+          this[`${what}Check`] = false;
+          this.translate.get('Sign.up.checkf').subscribe(comment => this.toast.error(`${what} ${comment}`));
+        }
+      );
+    } else {
+      this.translate.get('Sign.up.checkw').subscribe(comment => this.toast.error(comment));
+    }
+  }
+
   private signUp = (username: HTMLInputElement, email: HTMLInputElement, password: HTMLInputElement, rpassword: HTMLInputElement) => {
     if (
       !this.nError ||
@@ -101,9 +140,15 @@ export class SignupComponent implements AfterViewInit {
       username.value === '' ||
       email.value === '' ||
       password.value === '' ||
-      rpassword.value === ''
+      rpassword.value === '' ||
+      !this.usernameCheck ||
+      !this.emailCheck
     ) {
-      this.toast.error('Not right value !');
+      if (!this.usernameCheck || !this.emailCheck) {
+        this.translate.get('Sign.up.notchecked').subscribe(comment => this.toast.error(comment));
+      } else {
+        this.translate.get('Sign.up.notright').subscribe(comment => this.toast.error(comment));
+      }
     } else {
       this.store.dispatch(
         new SignActions.SignUp({
