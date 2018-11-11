fd5b9f984f5eaf057fdc3f600b6234c3c57ae8b9
NoMoreViolence <ljh86029926@gmail.com>
2018-11-11 15:55:47 +0900

    Frontend-Add http request method

--- maker-frontend/src/app/services/sign.service.ts
+++ maker-frontend/src/app/services/sign.service.ts
@@ -5,10 +5,16 @@ import { Observable, of } from 'rxjs';
 import { HttpResponse, Omit } from '../interface';
 
 interface Login {
-  admin?: boolean;
-  username?: string;
-  email?: string;
-  token?: string;
+  admin: boolean;
+  username: string;
+  email: string;
+  token: string;
+}
+
+interface AutoLogin {
+  admin: boolean;
+  username: string;
+  email: string;
 }
 
 interface Register {
@@ -29,11 +35,18 @@ export class SignService {
       catchError((err: { error: Omit<HttpResponse, 'value'> }) => of(err.error))
     )
 
-  public signIn = (email: string, password: string): Observable<Omit<HttpResponse, 'value'> & { value?: Login }> =>
+  public autoSignIn = (token: string): Observable<Omit<HttpResponse, 'value'> & { value: AutoLogin }> =>
+    this.http.post('/api/auth/check', {}, { headers: { jwttoken: token } }).pipe(
+      first(),
+      map((res: Omit<HttpResponse, 'value'> & { value: AutoLogin }) => res),
+      catchError((err: { error: HttpResponse }) => of({ ...err.error, value: { username: '', email: '', admin: false } }))
+    )
+
+  public signIn = (email: string, password: string): Observable<Omit<HttpResponse, 'value'> & { value: Login }> =>
     this.http.post('/api/auth/login', { email, password }).pipe(
       first(),
-      map((res: Omit<HttpResponse, 'value'> & { value?: Login }) => res),
-      catchError((err: { error: HttpResponse }) => of(err.error))
+      map((res: Omit<HttpResponse, 'value'> & { value: Login }) => res),
+      catchError((err: { error: HttpResponse }) => of({ ...err.error, value: { admin: false, username: '', email: '', token: '' } }))
     )
 
   public signUp = (username: string, email: string, password: string): Observable<Omit<HttpResponse, 'value'> & { value?: Register }> =>
