09ece2144d3ce61d8d752109653d753f8a67510a
NoMoreViolence <ljh86029926@gmail.com>
2018-11-11 15:28:05 +0900

    Frontend-Modify signin component (markup, rxjs, css)

--- maker-frontend/src/app/components/sign/signin/signin.component.ts
+++ maker-frontend/src/app/components/sign/signin/signin.component.ts
@@ -1,10 +1,9 @@
-import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
-import { fromEvent, Observable, combineLatest } from 'rxjs';
-import { debounceTime, catchError, tap, first } from 'rxjs/operators';
+import { fromEvent, Observable, Subscription } from 'rxjs';
+import { debounceTime, tap, first } from 'rxjs/operators';
 import { ToastrService } from 'ngx-toastr';
-
 import lib from '../../../lib';
 import { AppState, User } from '../../../ngrx/models';
 import { SignActions } from '../../../ngrx/actions';
@@ -15,27 +14,30 @@ import { TranslateService } from '@ngx-translate/core';
   templateUrl: './signin.component.html',
   styleUrls: ['./signin.component.scss']
 })
-export class SigninComponent implements AfterViewInit {
-  private eError = true;
-  private pError = true;
+export class SigninComponent implements AfterViewInit, OnDestroy {
+  public eError = true;
+  public pError = true;
   @ViewChild('em')
-  private email: ElementRef<HTMLInputElement>;
+  public email: ElementRef<HTMLInputElement>;
   @ViewChild('pw')
-  private password: ElementRef<HTMLInputElement>;
-  private user: Observable<User>;
+  public password: ElementRef<HTMLInputElement>;
+  public user$: Observable<User>;
+  public subscriptions: Subscription[] = [];
 
   constructor(private store: Store<AppState>, private router: Router, private translate: TranslateService, private toast: ToastrService) {
-    this.user = store.select('user');
+    this.user$ = store.select('user');
   }
 
   ngAfterViewInit() {
-    this.user.pipe(first()).subscribe(userData => {
+    this.user$.pipe(first()).subscribe(userData => {
       if (userData.success === true) {
-        combineLatest(this.router.navigateByUrl('/main'), this.translate.get('Sign.in.already'), (uri, translate) => {
-          this.toast.info(translate);
-        }).subscribe();
+        this.router.navigateByUrl('/main');
+        this.translate
+          .get('Sign.in.already')
+          .pipe(first())
+          .subscribe(comment => this.toast.info(comment));
       } else {
-        fromEvent(this.email.nativeElement, 'input')
+        const email = fromEvent(this.email.nativeElement, 'input')
           .pipe(
             tap(x => (this.eError = true)),
             debounceTime(600)
@@ -48,7 +50,7 @@ export class SigninComponent implements AfterViewInit {
             }
           });
 
-        fromEvent(this.password.nativeElement, 'input')
+        const password = fromEvent(this.password.nativeElement, 'input')
           .pipe(
             tap(x => (this.pError = true)),
             debounceTime(600)
@@ -60,13 +62,23 @@ export class SigninComponent implements AfterViewInit {
               this.pError = lib.passwordRegex.test(this.password.nativeElement.value);
             }
           });
+
+        this.subscriptions.push(email);
+        this.subscriptions.push(password);
       }
     });
   }
 
-  private signIn = (email: HTMLInputElement, password: HTMLInputElement) => {
+  ngOnDestroy() {
+    this.subscriptions.map(x => x.unsubscribe());
+  }
+
+  public signIn = (email: HTMLInputElement, password: HTMLInputElement) => {
     if (!this.eError || !this.pError || email.value === '' || password.value === '') {
-      this.translate.get('Sign.in.notchecked').subscribe(comment => this.toast.error(comment));
+      this.translate
+        .get('Sign.in.notright')
+        .pipe(first())
+        .subscribe(comment => this.toast.error(comment));
     } else {
       this.store.dispatch(new SignActions.SignIn({ email: email.value, password: password.value }));
     }
