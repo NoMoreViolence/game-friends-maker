439ec6dc9b7a307287d0d8e76edde568956b10a4
NoMoreViolence <ljh86029926@gmail.com>
2018-11-19 23:37:53 +0900

    Backend-Game to Public-api

--- maker-backend/src/routes/index.ts
+++ maker-backend/src/routes/index.ts
@@ -3,12 +3,12 @@ const Router = express.Router();
 
 import admin from './admin';
 import auth from './auth';
-import game from './game';
+import api from './public-api';
 import user from './user';
 
 Router.use('/admin', admin);
 Router.use('/auth', auth);
-Router.use('/game', game);
 Router.use('/user', user);
+Router.use('/api', api);
 
 export default Router;
--- maker-backend/src/routes/public-api/index.ts
+++ maker-backend/src/routes/public-api/index.ts
@@ -3,7 +3,10 @@ import * as Controller from './game.controller';
 
 const Router = express.Router();
 
-Router.get('/:game', Controller.getGame);
-Router.get('', Controller.getAllGame);
+Router.get('/game/:game', Controller.getGame);
+Router.get('/game', Controller.getAllGame);
+
+Router.get('/genre/:genre');
+Router.get('/genre');
 
 export default Router;
