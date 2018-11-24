a54409b442cc27ecd2383f97d3a163ed0fd7a76f
NoMoreViolence <ljh86029926@gmail.com>
2018-11-24 20:00:24 +0900

    Frontend-Eject + add scss compiler

--- maker-frontend/src/index.tsx
+++ maker-frontend/src/index.tsx
@@ -1,11 +1,8 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
+import './index.scss';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
-);
+ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
 registerServiceWorker();
