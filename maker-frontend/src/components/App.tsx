581ff12ced8979b406032caf729fd4ec442367ff
NoMoreViolence <ljh86029926@gmail.com>
2018-12-07 23:50:41 +0900

    Frontend-Main ui config

--- maker-frontend/src/components/App.tsx
+++ maker-frontend/src/components/App.tsx
@@ -1,20 +1,22 @@
 import * as React from 'react';
-import { Switch, Route, NavLink } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import HeaderPage from 'pages/header/header.page';
+import signPage from 'pages/sign/sign.page';
+
 import 'components/App.scss';
-import LoginPage from 'pages/login/login.page';
+import 'react-toastify/dist/ReactToastify.min.css';
 
-class App extends React.Component<any, any> {
-  public render() {
-    return (
-      <div id="main">
-        <div id="header">helo</div>
-        <NavLink to="/login">click Me</NavLink>
-        <Switch>
-          <Route path="/login" exact={true} component={LoginPage} />
-        </Switch>
-      </div>
-    );
-  }
-}
+const AppComponent: React.SFC<{}> = () => {
+  return (
+    <div id="app" lang="ko">
+      <ToastContainer />
+      <HeaderPage />
+      <Switch>
+        <Route path="/sign" component={signPage} />
+      </Switch>
+    </div>
+  );
+};
 
-export default App;
+export default AppComponent;
