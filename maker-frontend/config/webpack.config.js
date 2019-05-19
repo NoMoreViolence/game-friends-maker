8b21db5e5d70b41f5bf30b33d50bdf41ee2e859a
NoMoreViolence <ljh86029926@gmail.com>
2019-05-19 16:55:45 +0900

    Frontend - include media

--- maker-frontend/config/webpack.config.js
+++ maker-frontend/config/webpack.config.js
@@ -435,7 +435,8 @@ module.exports = function (webpackEnv) {
                 loader: require.resolve('sass-loader'),
                 options: {
                   includePaths: [paths.appSrc + '/styles'],
-                  sourceMap: isEnvProduction && shouldUseSourceMap
+                  sourceMap: isEnvProduction && shouldUseSourceMap,
+                  data: `@import '~include-media/dist/include-media';`
                 }
               }),
               // Don't consider CSS imports dead code even if the
