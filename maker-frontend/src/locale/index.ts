acbf089f2cbde296c561a1312af1c103010c2333
NoMoreViolence <ljh86029926@gmail.com>
2019-07-22 22:56:38 +0900

    Front - Add locale

--- maker-frontend/src/locale/index.ts
+++ maker-frontend/src/locale/index.ts
@@ -1,10 +1,11 @@
 import { mainLocale } from './main.locale';
 import { landingLocale } from './landing.locale';
 import { alertLocale } from './alert.locale';
+import { termsLocale } from './terms.locale';
 
 const locale = {
-  ko: { ...mainLocale.ko, ...alertLocale.ko, ...landingLocale.ko },
-  en: { ...mainLocale.en, ...alertLocale.en, ...landingLocale.en },
+  ko: { ...mainLocale.ko, ...alertLocale.ko, ...landingLocale.ko, ...termsLocale.ko },
+  en: { ...mainLocale.en, ...alertLocale.en, ...landingLocale.en, ...termsLocale.en },
 };
 
 export default locale;
--- maker-frontend/src/locale/landing.locale.ts
+++ maker-frontend/src/locale/landing.locale.ts
@@ -2,7 +2,7 @@ export const landingLocale = {
   ko: {
     'landing.main.message': '원하는 사람과 함께 게임하세요.',
     'landing.sub.message': 'Cooperative는 원하는 사람을 찾아 함께 게임을 즐길 수 있게 해 줍니다.',
-    'landing.startWithGoogleAgree': '버튼을 클릭하면 Cooperative 약관에 동의하게 됩니다.',
+    'landing.startWithGoogleAgree': "버튼을 클릭하면 'Cooperative 약관'에 동의하게 됩니다.",
     'landing.startWithGoogle': '구글 계정으로 로그인 or 가입',
   },
   en: {
