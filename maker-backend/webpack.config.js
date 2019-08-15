62c78bdefcbf152649c44165ab5270a0aa6ea493
NoMoreViolence <ljh86029926@gmail.com>
2019-08-16 00:03:57 +0900

    Backend - Serverless setting fin

--- maker-backend/webpack.config.js
+++ maker-backend/webpack.config.js
@@ -1,49 +1,28 @@
-const fs = require('fs');
-const path = require('path');
-const slsw = require('serverless-webpack');
-const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
-const TerserWebpackPlugin = require('terser-webpack-plugin');
+var path = require('path');
+var slsw = require('serverless-webpack');
+var nodeExternals = require('webpack-node-externals');
+var TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 module.exports = {
-  ...(!slsw.lib.webpack.isLocal ? {
-    optimization: {
-      minimizer: [
-        new TerserWebpackPlugin({
-          parallel: true, // Enable/disable multi-process parallel running.
-        }),
-      ],
-    }
-  } : {}),
-  // plugins: [
-  //   new CompressionPlugin({
-  //     compressionOptions: {
-  //       numiterations: 15,
-  //     },
-  //     algorithm(input, compressionOptions, callback) {
-  //       return zopfli.gzip(input, compressionOptions, callback);
-  //     },
-  //   }),
-  // ],
   mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
   entry: slsw.lib.entries,
   devtool: 'source-map',
+  externals: [nodeExternals()],
   resolve: {
-    plugins: [new TsconfigPathsPlugin()],
-    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx', '.mjs'],
+    modules: [path.resolve('./src'), 'node_modules'],
+    plugins: [new TsconfigPathsPlugin({ configFile: './tsconfig.json' })],
+    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
   },
   output: {
     libraryTarget: 'commonjs',
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, '.webpack'),
     filename: '[name].js',
   },
   target: 'node',
   module: {
     rules: [
-      {
-        test: /\.tsx?$/,
-        exclude: /node_modules/,
-        use: ['ts-loader', 'eslint-loader'],
-      },
+      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+      { test: /\.tsx?$/, use: ['ts-loader', 'eslint-loader'] },
     ],
   },
 };
