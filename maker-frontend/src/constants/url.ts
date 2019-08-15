d78eaa211ae182548a98521729ec5ad8f6da2da3
NoMoreViolence <ljh86029926@gmail.com>
2019-08-16 03:35:17 +0900

    Frontend - API endpoint set

--- maker-frontend/src/constants/url.ts
+++ maker-frontend/src/constants/url.ts
@@ -1,17 +1,17 @@
 import axios, { AxiosInstance } from 'axios';
 
 export const localServerUrl = 'http://localhost:3001';
-export const devServerUrl = 'https://dev-server.coopnow.net';
-export const prodServerUrl = 'https://prod-server.coopnow.net';
+export const devServerUrl = 'https://dev-server.coopnow.net/dev';
+export const prodServerUrl = 'https://prod-server.coopnow.net/prod';
 
 export const detectEnvironment = (): AxiosInstance => {
   const url = new URL(document.URL);
 
   if (url.host.includes('localhost') || url.host.includes('127.0.0.1')) {
     return axios.create({ baseURL: localServerUrl });
-  } else if (url.host === 'dev-server.coopnow.net') {
+  } else if (url.host === 'coopnow.net') {
     return axios.create({ baseURL: devServerUrl });
-  } else if (url.host === 'prod-server.coopnow.net') {
+  } else if (url.host === 'cohope.io') {
     return axios.create({ baseURL: prodServerUrl });
   } else {
     return axios.create({ baseURL: localServerUrl });
