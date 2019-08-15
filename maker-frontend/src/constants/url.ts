e58ba658b754b28e762d163743d1541cd43db120
NoMoreViolence <ljh86029926@gmail.com>
2019-08-16 00:57:55 +0900

    Frontend - Apply prod & dev server url

--- maker-frontend/src/constants/url.ts
+++ maker-frontend/src/constants/url.ts
@@ -1,14 +1,19 @@
 import axios, { AxiosInstance } from 'axios';
 
-export const devServerUrl = 'http://localhost:3001';
-export const serverUrl = 'FuckingUrl';
+export const localServerUrl = 'http://localhost:3001';
+export const devServerUrl = 'https://dev-server.coopnow.net';
+export const prodServerUrl = 'https://prod-server.coopnow.net';
 
 export const detectEnvironment = (): AxiosInstance => {
   const url = new URL(document.URL);
 
-  if (url.host.includes('localhost')) {
+  if (url.host.includes('localhost') || url.host.includes('127.0.0.1')) {
+    return axios.create({ baseURL: localServerUrl });
+  } else if (url.host === 'dev-server.coopnow.net') {
     return axios.create({ baseURL: devServerUrl });
+  } else if (url.host === 'prod-server.coopnow.net') {
+    return axios.create({ baseURL: prodServerUrl });
   } else {
-    return axios.create({ baseURL: devServerUrl });
+    return axios.create({ baseURL: localServerUrl });
   }
 };
