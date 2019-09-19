37f6b6a1707ddf75880f7c2c1882768cdf705932
NoMoreViolence <ljh86029926@gmail.com>
2019-09-20 03:06:18 +0900

    Frontend: Add error message in sign saga logic

---
+++ maker-frontend/src/lib/common.error.toast.ts
@@ -0,0 +1,45 @@
+import { globalActions } from '@actions';
+
+export const error401Toast = () =>
+  globalActions.toast({
+    type: 'error',
+    title: 'error.401.title',
+    text: 'error.401.comment',
+  });
+export const error400Toast = () =>
+  globalActions.toast({
+    type: 'error',
+    title: 'error.400.title',
+    text: 'error.400.comment',
+  });
+export const error500Toast = () =>
+  globalActions.toast({
+    type: 'error',
+    title: 'error.500.title',
+    text: 'error.500.comment',
+  });
+
+export const error401Alert = () =>
+  globalActions.alert({
+    type: 'error',
+    title: 'error.401.title',
+    text: 'error.401.comment',
+    showConfirmButton: true,
+    confirmText: 'common.close',
+  });
+export const error400Alert = () =>
+  globalActions.alert({
+    type: 'error',
+    title: 'error.400.title',
+    text: 'error.400.comment',
+    showConfirmButton: true,
+    confirmText: 'common.close',
+  });
+export const error500Alert = () =>
+  globalActions.alert({
+    type: 'error',
+    title: 'error.500.title',
+    text: 'error.500.comment',
+    showConfirmButton: true,
+    confirmText: 'common.close',
+  });
