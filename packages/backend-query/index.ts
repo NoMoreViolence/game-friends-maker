3ef685ed9e8138345a7d87f02d062ae1d8c9483d
NoMoreViolence <ljh86029926@gmail.com>
2019-10-30 01:40:43 +0900

    Backend-query(feat&fix): create genre create function, fix mongoose warning & process argv issue

--- packages/backend-query/index.ts
+++ packages/backend-query/index.ts
@@ -4,36 +4,37 @@ import { Game, Genre, dbConnect } from './src';
 const game = new Game();
 const genre = new Genre();
 
+// process argv [0] [1] === nvm and node info
 dbConnect().then(({ success }) => {
   if (success) {
-    if (process.argv[0] === 'game') {
-      if (process.argv[1] === 'create') {
+    if (process.argv[2] === 'game') {
+      if (process.argv[3] === 'create') {
         game.create();
         return;
       }
-      if (process.argv[1] === 'update') {
+      if (process.argv[3] === 'update') {
         game.update();
         return;
       }
-      if (process.argv[1] === 'delete') {
+      if (process.argv[3] === 'delete') {
         game.delete();
         return;
       }
-    } else if (process.argv[0] === 'genre') {
-      if (process.argv[1] === 'create') {
+    } else if (process.argv[2] === 'genre') {
+      if (process.argv[3] === 'create') {
         genre.create();
         return;
       }
-      if (process.argv[1] === 'update') {
+      if (process.argv[3] === 'update') {
         genre.update();
         return;
       }
-      if (process.argv[1] === 'delete') {
+      if (process.argv[3] === 'delete') {
         genre.delete();
         return;
       }
     }
   } else {
-    //
+    console.log('failure');
   }
 });
