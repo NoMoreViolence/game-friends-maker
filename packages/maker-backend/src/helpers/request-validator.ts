a81eb7ad355f106ce956e93a5e4fdf879a780ae0
NoMoreViolence <ljh86029926@gmail.com>
2019-11-05 00:26:40 +0900

    Backend(chore): clean

--- packages/maker-backend/src/helpers/request-validator.ts
+++ packages/maker-backend/src/helpers/request-validator.ts
@@ -3,13 +3,7 @@ import { HttpStatusCode } from '@constants';
 import { NewError } from '@helpers';
 
 export const requestValidator = <T>(schema: Joi.ObjectSchema, validateParams: T) => {
-  const {
-    error,
-    value,
-  }: {
-    error: Joi.ValidationError;
-    value: T;
-  } = schema.validate(validateParams);
+  const { error, value } = schema.validate<T>(validateParams);
 
   if (error) {
     throw new NewError(HttpStatusCode.BAD_REQUEST);
