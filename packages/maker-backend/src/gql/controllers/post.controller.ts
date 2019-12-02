d2c577d1947a56e4907860e976b31271005c2f75
NoMoreViolence <ljh86029926@gmail.com>
2019-12-03 01:29:42 +0900

    Backend(refactor): Add unpopulate option

--- packages/maker-backend/src/gql/controllers/post.controller.ts
+++ packages/maker-backend/src/gql/controllers/post.controller.ts
@@ -1,41 +1,53 @@
 import { Service } from 'typedi';
 import { ObjectId } from 'mongodb';
-import { PostDocument, UserDocument, UserModel, PostModel } from '@common-server';
-import { PostService, CommonService } from '@gql/services';
+import { PostDocument, UserDocument, UserModel } from '@common-server';
+import { PostService, CommonService, UserService } from '@gql/services';
 import { AuthenticationError, ApolloError } from 'apollo-server';
-import { UpdatePostPayload } from '@gql/payloads';
+import { UpdatePostPayload, CreatePostPayload } from '@gql/payloads';
 
 @Service()
 export class PostController {
-  constructor(private postService: PostService, private commonService: CommonService) {}
+  constructor(
+    private postService: PostService,
+    private userService: UserService,
+    private commonService: CommonService,
+  ) {}
+
+  public async createPost(user: UserDocument, payload: CreatePostPayload) {
+    this.userService.checkUserCanJoinPost(user);
+    const post = await this.postService.createPost(user._id, payload);
+    await UserModel.findOneAndUpdate({ _id: user._id }, { $addToSet: { posts: post._id } }, { new: true }).exec();
+    return post;
+  }
 
   public async updatePost(user: UserDocument, postId: ObjectId, nextPost: UpdatePostPayload) {
-    const nullablePost = await this.postService.getPostById(new ObjectId(postId));
+    const nullablePost = await this.postService.getPostById(new ObjectId(postId), false);
     const post = this.isOwner(user, nullablePost);
     return this.postService.updatePost(post, nextPost);
   }
 
   public async joinPost(requestee: UserDocument, postId: ObjectId) {
-    const post = await this.postService.getPostById(postId);
+    this.userService.checkUserCanJoinPost(requestee);
+    const post = await this.postService.getPostById(postId, false);
     if (!post) {
       throw new ApolloError('There is no data');
     }
-    if (post.authorId instanceof UserModel && post.authorId._id.equals(requestee._id)) {
+    if (post.authorId.equals(requestee._id)) {
       throw new AuthenticationError('You cannot join this party');
     }
 
-    return this.postService.joinPost(postId, requestee._id) as Promise<PostDocument>;
+    return this.postService.joinPost(postId, requestee._id);
   }
 
   public async deletePost(user: UserDocument, postId: ObjectId) {
-    const nullablePost = await this.postService.getPostById(new ObjectId(postId));
+    const nullablePost = await this.postService.getPostById(new ObjectId(postId), false);
     const post = this.isOwner(user, nullablePost);
     return this.postService.deletePost(post);
   }
 
   private isOwner(user: UserDocument, nullablePost: PostDocument | null) {
     const post = this.commonService.nullable<PostDocument>(nullablePost);
-    if (post.authorId instanceof UserModel && !post.authorId._id.equals(user._id)) {
+    if (!post.authorId.equals(user._id)) {
       throw new AuthenticationError('You are not owner of this post');
     }
     return post;
--- packages/maker-backend/src/gql/services/user.service.ts
+++ packages/maker-backend/src/gql/services/user.service.ts
@@ -1,32 +1,40 @@
 import { Service } from 'typedi';
-import { UserModel, DBUser } from '@common-server';
+import { UserModel, DBUser, UserDocument } from '@common-server';
 import { Context } from '@gql/bootstrap/session';
-import { AuthenticationError } from 'apollo-server-lambda';
+import { AuthenticationError, ApolloError } from 'apollo-server-lambda';
 
 @Service()
 export class UserService {
-  public async getUserById(id: string) {
-    return UserModel.findById(id).exec();
+  public async getUserById(id: string, autopopulate = true) {
+    return UserModel.findById(id, {}, { autopopulate }).exec();
   }
 
-  public async getUser(args: Partial<DBUser>) {
-    return UserModel.findOne(args).exec();
+  public async getUser(args: Partial<DBUser>, autopopulate = true) {
+    return UserModel.findOne(args, {}, { autopopulate }).exec();
   }
 
-  public async getUsers(args: Partial<DBUser>) {
-    return UserModel.find(args).exec();
+  public async getUsers(args: Partial<DBUser>, autopopulate = true) {
+    return UserModel.find(args, {}, { autopopulate }).exec();
   }
 
-  public async getUserByContext(context: Context) {
+  public async getUserByContext(context: Context, autopopulate = true) {
     if (!context.session._id) {
       throw new AuthenticationError('user does not exist');
     }
 
-    const user = await this.getUserById(context.session._id);
+    const user = await this.getUserById(context.session._id, autopopulate);
     if (!user) {
       throw new AuthenticationError('user does not exist');
     }
 
+    return user;
+  }
+
+  public checkUserCanJoinPost(user: UserDocument) {
+    const userMaxTeamLength = user.posts.length + user.relatedTeams.length + user.pendingTeams.length;
+    if (userMaxTeamLength >= 5) {
+      throw new ApolloError("You can't create because you belong to more than 5 teams");
+    }
     return user;
   }
 }
