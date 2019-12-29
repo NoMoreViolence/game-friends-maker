e0387586ca1beb1df901d387d1f972258a94decd
NoMoreViolence <ljh86029926@gmail.com>
2019-12-30 04:51:24 +0900

    chore(backend-query): multiple creation

--- packages/backend-query/src/game.ts
+++ packages/backend-query/src/game.ts
@@ -1,32 +1,34 @@
 import { game as gameConstants } from './constants/target.json';
-import { GenreModel, GameModel } from './common-server';
+import { GameModel } from './common-server';
 
 export class Game {
   public async create() {
     try {
-      const isDuplicate = await GameModel.find({ name: gameConstants.create.gameName }).exec();
-      if (isDuplicate.length !== 0) {
-        throw new Error('Game !');
-      }
+      gameConstants.create.gameName.map(async name => {
+        const isDuplicate = await GameModel.find({ name }).exec();
+        if (isDuplicate.length !== 0) {
+          throw new Error('Game !');
+        }
 
-      const isNotDuplicateGenres = (w: string[]): boolean => new Set(w).size !== w.length;
-      if (isNotDuplicateGenres(gameConstants.create.genre)) {
-        throw new Error('Genre !');
-      }
+        // const isNotDuplicateGenres = (w: string[]): boolean => new Set(w).size !== w.length;
+        // if (isNotDuplicateGenres(gameConstants.create.genre)) {
+        //   throw new Error('Genre !');
+        // }
 
-      const genreExist = await Promise.all(
-        gameConstants.create.genre.map(async genreName => await GenreModel.find({ name: genreName }).exec()),
-      );
-      if (genreExist.filter(genre => genre.length !== 0).length !== gameConstants.create.genre.length) {
-        throw new Error('Genre !');
-      }
+        // const genreExist = await Promise.all(
+        //   gameConstants.create.genre.map(async genreName => await GenreModel.find({ name: genreName }).exec()),
+        // );
+        // if (genreExist.filter(genre => genre.length !== 0).length !== gameConstants.create.genre.length) {
+        //   throw new Error('Genre !');
+        // }
 
-      const genreIds = genreExist.map(genre => genre[0]._id.toString());
-      const generatedGame = new GameModel({ name: gameConstants.create.gameName, genres: genreIds });
-      const dbGame = await generatedGame.save();
+        // const genreIds = genreExist.map(genre => genre[0]._id.toString());
+        const generatedGame = new GameModel({ name });
+        const dbGame = await generatedGame.save();
 
-      console.log('Success');
-      console.log(dbGame.name, dbGame.genres);
+        console.log('Success');
+        console.log(dbGame.name, dbGame.genres);
+      });
     } catch (e) {
       console.log('There is an error ^ㅗ^');
     }
