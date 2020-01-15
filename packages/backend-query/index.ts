27fb7f7f4d7c4216c0d313cf276cde6a70b45cc5
NoMoreViolence <ljh86029926@gmail.com>
2020-01-16 01:29:32 +0900

    feat(backend-query): create cleandb script

--- packages/backend-query/index.ts
+++ packages/backend-query/index.ts
@@ -1,12 +1,18 @@
 require('dotenv').config();
-import { Game, Genre, dbConnect } from './src';
+import { Game, Genre, Clean, dbConnect } from './src';
 
 const game = new Game();
 const genre = new Genre();
+const cleanDb = new Clean();
 
 // process argv [0] [1] === nvm and node info
 dbConnect().then(({ success }) => {
   if (success) {
+    if (process.argv[2] === 'clean_withoutuser') {
+      cleanDb.deleteTeamAndTeamUserJoin();
+      return;
+    }
+
     if (process.argv[2] === 'game') {
       if (process.argv[3] === 'create') {
         game.create();
