e9d339e9ff1a74b61e2af177b30a240a6b985e55
NoMoreViolence <ljh86029926@gmail.com>
2020-02-16 06:36:12 +0900

    chore: change file name

--- packages/maker-frontend/src/helpers/index.ts
+++ packages/maker-frontend/src/helpers/index.ts
@@ -5,5 +5,5 @@ export * from './use-debounce';
 export * from './use-detect-team-user-join-id';
 export * from './use-detect-user-channel-join-id';
 export * from './use-focus-and-focus-out';
-export * from './use-get-prev-value';
+export * from './use-previous';
 export * from './use-router';
