0edbfa568cd5ca6b1257b78325f96d45bb1c3b03
NoMoreViolence <ljh86029926@gmail.com>
2020-05-27 00:49:02 +0900

    fix: deployment process

--- packages/maker-frontend/src/constants-frontend/url.ts
+++ packages/maker-frontend/src/constants-frontend/url.ts
@@ -1,16 +1,16 @@
 import axios, { AxiosInstance } from 'axios';
 
-export const localServerUrl = 'http://localhost:7000/local';
-export const devServerUrl = 'https://dev-server.coopnow.net/dev';
-export const prodServerUrl = 'https://prod-server.coopnow.net/prod';
+const localServerUrl = 'http://localhost:7000/local';
+const testServerUrl = 'https://staging-backend.cohope.io';
+const prodServerUrl = 'https://prod-server.coopnow.net/prod';
 
 export const getRestAxios = (): AxiosInstance => {
   const url = new URL(document.URL);
 
   if (url.host.includes('localhost') || url.host.includes('127.0.0.1')) {
     return axios.create({ baseURL: localServerUrl });
-  } else if (url.host === 'coopnow.net') {
-    return axios.create({ baseURL: devServerUrl });
+  } else if (url.host === 'staging.cohope.io') {
+    return axios.create({ baseURL: testServerUrl });
   } else if (url.host === 'cohope.io') {
     return axios.create({ baseURL: prodServerUrl });
   } else {
