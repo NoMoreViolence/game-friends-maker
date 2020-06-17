ad665169ea7ee47b0e626b340d5b57284fb486b6
NoMoreViolence <ljh86029926@gmail.com>
2020-06-18 00:01:35 +0900

    chore: getRestAxios to httpClient

--- packages/maker-frontend/src/constants-frontend/url.ts
+++ packages/maker-frontend/src/constants-frontend/url.ts
@@ -1,19 +1,27 @@
-import axios, { AxiosInstance } from 'axios';
+import axios from 'axios';
 
 const localServerUrl = 'http://localhost:7000/local';
 const testServerUrl = 'https://staging-backend.cohope.io';
-const prodServerUrl = 'https://prod-server.coopnow.net/prod';
+const prodServerUrl = 'https://backend.cohope.io';
 
-export const getRestAxios = (): AxiosInstance => {
+const getBackendUrl = (): string => {
   const url = new URL(document.URL);
 
   if (url.host.includes('localhost') || url.host.includes('127.0.0.1')) {
-    return axios.create({ baseURL: localServerUrl });
+    return localServerUrl;
   } else if (url.host === 'staging.cohope.io') {
-    return axios.create({ baseURL: testServerUrl });
+    return testServerUrl;
   } else if (url.host === 'cohope.io') {
-    return axios.create({ baseURL: prodServerUrl });
+    return prodServerUrl;
   } else {
-    return axios.create({ baseURL: localServerUrl });
+    return localServerUrl;
   }
 };
+
+export const httpClient = axios.create({
+  baseURL: getBackendUrl(),
+  validateStatus: (status) => status < 500,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
