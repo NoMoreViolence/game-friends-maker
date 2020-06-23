1f1d1d1291dea2bb5d02cf07c0a22cfd56ea64f2
NoMoreViolence <ljh86029926@gmail.com>
2020-06-24 00:07:15 +0900

    chore: import change

--- packages/maker-frontend/src/index.tsx
+++ packages/maker-frontend/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Root } from './bootstrap';
+import { Root } from 'bootstrap';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Root />, document.getElementById('root'));
