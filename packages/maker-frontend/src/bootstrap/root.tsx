e30686cc35fac73521d3bbf6edd950ae95bff392
NoMoreViolence <ljh86029926@gmail.com>
2020-06-27 03:09:27 +0900

    feat: add style & route

--- packages/maker-frontend/src/bootstrap/root.tsx
+++ packages/maker-frontend/src/bootstrap/root.tsx
@@ -1,7 +1,9 @@
 import { ApolloProvider } from '@apollo/react-hooks';
 import { Landing } from 'pages/Landing';
 import React, { FC, lazy, Suspense } from 'react';
-import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { ResetCss, theme } from 'ui/System';
 import { ApolloMainClient } from './apollo';
 
 const App = lazy(() => import('pages/App'));
@@ -9,13 +11,16 @@ const App = lazy(() => import('pages/App'));
 export const Root: FC = () => (
   <ApolloProvider client={ApolloMainClient}>
     <BrowserRouter>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Switch>
-          <Route to="/app" component={App} />
-          <Route to="/" component={Landing} />
-          <Redirect from="/*" to="/" />
-        </Switch>
-      </Suspense>
+      <ThemeProvider theme={theme}>
+        <ResetCss />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Switch>
+            <Route path="/app" component={App} />
+            <Route path="/" component={Landing} />
+            <Redirect from="/*" to="/" />
+          </Switch>
+        </Suspense>
+      </ThemeProvider>
     </BrowserRouter>
   </ApolloProvider>
 );
