2d7bdb6f6f5e84b6fab4e7fb9ea6e59682769d60
NoMoreViolence <ljh86029926@gmail.com>
2020-06-26 23:41:48 +0900

    feat: wrapping memo

--- packages/maker-frontend/src/ui/Components/Icons/Icons.tsx
+++ packages/maker-frontend/src/ui/Components/Icons/Icons.tsx
@@ -1,43 +1,57 @@
-import React, { FC } from 'react';
+import React, { FC, memo, useMemo } from 'react';
 import styled from 'styled-components';
 import { background, BackgroundProps, margin, MarginProps } from 'styled-system';
 import { Colors } from 'ui/Colors';
+import { cursor, CursorProps, transform, TransformProps } from 'ui/System';
 import { SvgIconProps } from './Interface';
 import { svgMap } from './SvgMap';
 
-export type IconName = keyof typeof svgMap;
-
-interface Props extends MarginProps, SvgIconProps, BackgroundProps {
+interface Props extends SvgIconProps, MarginProps, BackgroundProps, CursorProps, TransformProps {
   iconName: IconName;
-  backgroundColor?: string;
   style?: React.CSSProperties;
 }
-
-const Container = styled.div<MarginProps & BackgroundProps>`
-  ${margin}
-  ${background}
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-`;
-
-export const Icon: FC<Props> = ({
+const IconComponent: FC<Props> = ({
   iconName,
   size = 24,
   fillColor = Colors.black,
   opacity,
+  margin,
   mr,
   ml,
   mt,
   mb,
   style,
   background,
+  cursor,
+  transform,
 }) => {
-  const SvgIcon = svgMap[iconName];
-
+  const SvgIcon = useMemo(() => svgMap[iconName], [iconName]);
   return (
-    <Container mr={mr} ml={ml} mt={mt} mb={mb} style={style} background={background}>
+    <Container
+      margin={margin}
+      mr={mr}
+      ml={ml}
+      mt={mt}
+      mb={mb}
+      style={style}
+      background={background}
+      cursor={cursor}
+      transform={transform}
+    >
       <SvgIcon size={size} fillColor={fillColor} opacity={opacity} />
     </Container>
   );
 };
+
+const Container = styled.div<MarginProps & BackgroundProps & CursorProps & TransformProps>`
+  ${margin}
+  ${background}
+  ${cursor}
+  ${transform}
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+export type IconName = keyof typeof svgMap;
+export const Icon = memo(IconComponent);
