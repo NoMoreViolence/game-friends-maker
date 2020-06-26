91f8b0cac3e208dd73bb07a25b6e96b054d8ca12
NoMoreViolence <ljh86029926@gmail.com>
2020-06-27 03:08:42 +0900

    feat: add global stylesheet

---
+++ packages/maker-frontend/src/ui/System/Theme.ts
@@ -0,0 +1,23 @@
+// only root.ts use this file
+
+export const theme = {
+  breakpoints: ['0px', '768px', '1024px', '1444px'],
+};
+
+/*
+  // Styled System outputs this style object
+  {
+    @media screen and (min-width: 0px): {
+      
+    }
+    @media screen and (min-width: 768px): {
+
+    }
+    @media screen and (min-width: 1024px): {
+
+    }
+    @media screen and (min-width: 1444px): {
+
+    }
+  }
+*/
